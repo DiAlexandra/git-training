b78aaa4309285672f28d5325e735e4c02a944b3b
Alexandra Diallo <al.diallocotrung@gmail.com>
2025-03-06 15:42:50 +0000

    suppressed old example file

--- models/staging/finance/stg_finance__campaign_days.sql
+++ models/staging/finance/stg_finance__campaign_days.sql
@@ -9,6 +9,20 @@ source as (
 renamed as (
 
     select
+        date_date,
+        average_basket,
+        operational_margin,
+        ads_cost,
+        ads_impression,
+        ads_click,
+        total_quantity,
+        total_revenue,
+        total_purchase_cost,
+        margin,
+        total_shipping_fees,
+        total_log_cost,
+        total_ship_cost,
+        ads_margin
 
     from source
 
